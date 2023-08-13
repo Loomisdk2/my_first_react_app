0f6b779806bfd69a595f22475e0e32afae1dc9a5
Douglas Loomis <125160013+Loomisdk2@users.noreply.github.com>
2023-08-13 12:51:00 -0500

    New files added. Working on App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useEffect } from "react";
 
 import "./App.css";
+import SearchIcon from "./search.svg";
+
+const API_URL = "http://www.omdbapi.com?apikey=eccc8782";
 
 const App = () => {
-  return;
-  <div className="App"></div>;
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Superman");
+  }, []);
+  return <h1>App</h1>;
 };
 
 export default App;
